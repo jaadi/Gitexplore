42ebc9352a096e5f535187b05a1fae8633de5cba
Jaadi <naimgoog@gmail.com>
2015-06-06 15:46:06 +0200

    initial commit

---
+++ GitExplore/src/pac1/A.java
@@ -0,0 +1,21 @@
+package pac1;
+
+public class A {
+
+	public A(){
+		
+		sayHallo();
+		
+	}
+	public static void main(String[] args) {
+		new A();
+
+	}
+	
+	public void sayHallo(){
+		
+		System.out.println("hallo from class A");
+		
+	}
+
+}
